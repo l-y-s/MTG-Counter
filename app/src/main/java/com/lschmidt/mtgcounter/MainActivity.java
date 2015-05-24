f8bf33dff41d3a8ac69f65d045df3830f8da435b
l-y-s <loganschmidt94@gmail.com>
2015-05-24 15:40:24 -0700

    Added two-player support. Functions in landscape mode, portrait mode broken.

--- app/src/main/java/com/lschmidt/mtgcounter/MainActivity.java
+++ app/src/main/java/com/lschmidt/mtgcounter/MainActivity.java
@@ -1,9 +1,11 @@
 package com.lschmidt.mtgcounter;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -42,8 +44,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -51,14 +54,13 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_twoPlayer:
+                switchTwoPlayer();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void decrement(View view) {
@@ -83,5 +85,10 @@ public class MainActivity extends ActionBarActivity {
         counter = counter + 5;
         final TextView life = (TextView) findViewById(R.id.life);
         life.setText(Integer.toString(counter));
+    }
+
+    public void switchTwoPlayer(){
+        Intent intent = new Intent(this, TwoPlayer.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/lschmidt/mtgcounter/TwoPlayer.java
@@ -0,0 +1,119 @@
+package com.lschmidt.mtgcounter;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class TwoPlayer extends ActionBarActivity {
+
+    int counter1 = 20;
+    int counter2 = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_player);
+    }
+
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("playerOneLife", counter1);
+        outState.putInt("playerTwoLife", counter2);
+    }
+
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+
+        counter1 = savedInstanceState.getInt("playerOneLife");
+        counter2 = savedInstanceState.getInt("playerTwoLife");
+        final TextView life1 = (TextView) findViewById(R.id.life1);
+        final TextView life2 = (TextView) findViewById(R.id.life2);
+        life1.setText((Integer.toString(counter1)));
+        life2.setText(Integer.toString(counter2));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_two_player, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()){
+            case R.id.action_onePlayer:
+                switchOnePlayer();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void decrementLife1 (View view){
+        counter1--;
+        final TextView life1 = (TextView) findViewById(R.id.life1);
+        life1.setText(Integer.toString(counter1));
+    }
+
+    public void decrement5Life1 (View view){
+        counter1 = counter1 - 5;
+        final TextView life1 = (TextView) findViewById(R.id.life1);
+        life1.setText(Integer.toString(counter1));
+    }
+
+    public void decrementLife2 (View view){
+        counter2--;
+        final TextView life2 = (TextView) findViewById(R.id.life2);
+        life2.setText(Integer.toString(counter2));
+    }
+
+    public void decrement5Life2 (View view){
+        counter2 = counter2 - 5;
+        final TextView life2 = (TextView) findViewById(R.id.life2);
+        life2.setText(Integer.toString(counter2));
+    }
+
+    public void incrementLife1 (View view){
+        counter1++;
+        final TextView life1 = (TextView) findViewById(R.id.life1);
+        life1.setText(Integer.toString(counter1));
+    }
+
+    public void increment5Life1 (View view){
+        counter1 = counter1 + 5;
+        final TextView life1 = (TextView) findViewById(R.id.life1);
+        life1.setText(Integer.toString(counter1));
+    }
+
+    public void incrementLife2 (View view){
+        counter2++;
+        final TextView life2 = (TextView) findViewById(R.id.life2);
+        life2.setText(Integer.toString(counter2));
+    }
+
+    public void increment5Life2 (View view) {
+        counter2 = counter2 + 5;
+        final TextView life2 = (TextView) findViewById(R.id.life2);
+        life2.setText(Integer.toString(counter2));
+    }
+
+    public void switchOnePlayer(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
